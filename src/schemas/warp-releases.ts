1f4de9736f1ae5de72dba27d32606fdb82717d25
Bankz1990 <187200704+Bankz1990@users.noreply.github.com>
2025-04-21 19:13:55 +0000

    Initialize kian/PCX-14722

---
+++ src/schemas/warp-releases.ts
@@ -0,0 +1,10 @@
+import { z } from "astro:schema";
+
+export const warpReleasesSchema = z.object({
+	version: z.string(),
+	releaseDate: z.coerce.date(),
+	releaseNotes: z.string(),
+	packageSize: z.number(),
+	packageURL: z.string(),
+	platformName: z.enum(["Windows", "macOS"]),
+});
